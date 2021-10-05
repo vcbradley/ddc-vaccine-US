b80eaf8f570c58cc09eea1710b85ca87ae3aa2d8
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-10-05 00:04:33 -0700

    Style file

--- functions/functions_clean_axios-ipsos.R
+++ functions/functions_clean_axios-ipsos.R
@@ -5,36 +5,45 @@ read_ax_drop <- function(x) {
   haven::read_sav(x) %>%
     select(-ppmsacat) %>%
     mutate_if(is.labelled, as_factor) %>%
-    mutate(date = str_remove_all(path_file(x), "(Axios-Ipsos\\s|Data\\.sav)"),
-           date = str_c(date, ", 2021"),
-           date = mdy(date)
+    mutate(
+      date = str_remove_all(path_file(x), "(Axios-Ipsos\\s|Data\\.sav)"),
+      date = str_c(date, ", 2021"),
+      date = mdy(date)
     ) %>%
     mutate(date = replace(date, str_detect(x, "Axios W42 032221.sav"), ymd("2021-03-22")))
 }
 
 
 #' This data is one row per microdata
-cleanIPSOSdata = function(){
-
+cleanIPSOSdata <- function() {
   axios_dates <- c("April 19", "April 5", "March 1", "March 22", "March 8", "May 10")
-  axios_paths <- c(glue("Axios-Ipsos {axios_dates} Data.tab"),
-                   "Axios W42 032221.tab")
+  axios_paths <- c(
+    glue("Axios-Ipsos {axios_dates} Data.tab"),
+    "Axios W42 032221.tab"
+  )
 
-  ax_raw0 <-  map_dfr(
+  ax_raw0 <- map_dfr(
     .x = axios_paths,
     .f = function(x) {
       get_dataframe_by_name(
         filename = x,
         dataset = dvdoi,
-        original =  TRUE,
-        .f = read_ax_drop)
-    })
+        original = TRUE,
+        .f = read_ax_drop
+      )
+    }
+  )
 
   ax_raw <- ax_raw0 %>%
-    mutate(educ = fct_collapse(ppeduc5,
-                               `HS or Less` = c("No high school diploma or GED",
-                                                "High school graduate (high school diploma or the equivalent GED)")),
-           wt_final = coalesce(wt_final, wt_final_pid)) %>%
+    mutate(
+      educ = fct_collapse(ppeduc5,
+        `HS or Less` = c(
+          "No high school diploma or GED",
+          "High school graduate (high school diploma or the equivalent GED)"
+        )
+      ),
+      wt_final = coalesce(wt_final, wt_final_pid)
+    ) %>%
     relocate(date, WAVE, matches("wt_final"), matches("pp"))
 
   # code vax, hes, will as binary
@@ -45,7 +54,8 @@ cleanIPSOSdata = function(){
       willing = replace(willing, Q73 == "Skipped", NA_real_),
       willing = replace(willing, Q107_1 == "Yes, I have received the vaccine", 0),
       # vax is whether you got it
-      vaccinated = as.integer(Q107_1 == "Yes, I have received the vaccine")) %>%
+      vaccinated = as.integer(Q107_1 == "Yes, I have received the vaccine")
+    ) %>%
     # hesitant is the subtraction
     mutate(hesitant = 1 - vaccinated - willing) %>%
     relocate(date, WAVE, wt_final, willing, hesitant, vaccinated)
@@ -53,12 +63,12 @@ cleanIPSOSdata = function(){
 
   cat("Writing archived data")
   ax_coded %>%
-    filter(is.na(acsnet3)) %>%  # public data
+    filter(is.na(acsnet3)) %>% # public data
     write_rds("data/axios-ipsos/axios-ipsos_stacked.rds")
 
   cat("Writing offlineflag.rds")
   ax_coded %>%
-    filter(!is.na(acsnet3)) %>%  # custom offline flag
+    filter(!is.na(acsnet3)) %>% # custom offline flag
     write_rds("data/axios_ipsos/axios-ipsos_offlineflag.rds")
 }
 
@@ -66,8 +76,7 @@ cleanIPSOSdata = function(){
 
 #' This data is one row per estimate
 cleanIPSOStables <- function() {
-
-  ipsos_text = get_dataframe_by_name(
+  ipsos_text <- get_dataframe_by_name(
     "axios_ipsos_full.tab",
     dataset = dvdoi,
     original =  TRUE,
@@ -75,13 +84,14 @@ cleanIPSOStables <- function() {
   )
 
   ipsos <- ipsos_text %>%
-    mutate(source = 'toplines'
-           , pct_hesitant = 1 - pct_haveorwillgetvax
-           , pct_willing = pct_haveorwillgetvax - pct_vaccinated
-           , pct_vaccinated_se = sqrt(deff * pct_vaccinated * (1 - pct_vaccinated)/n)
-           , pct_hesitant_se = sqrt(deff * pct_hesitant * (1 - pct_hesitant)/n)
-           , pct_willing_se = sqrt(deff * pct_willing * (1 - pct_willing)/n)
+    mutate(
+      source = "toplines",
+      pct_hesitant = 1 - pct_haveorwillgetvax,
+      pct_willing = pct_haveorwillgetvax - pct_vaccinated,
+      pct_vaccinated_se = sqrt(deff * pct_vaccinated * (1 - pct_vaccinated) / n),
+      pct_hesitant_se = sqrt(deff * pct_hesitant * (1 - pct_hesitant) / n),
+      pct_willing_se = sqrt(deff * pct_willing * (1 - pct_willing) / n)
     )
 
-  write.csv(ipsos, file = 'data/final/axios_ipsos_cleaned.csv')
+  write.csv(ipsos, file = "data/final/axios_ipsos_cleaned.csv")
 }
